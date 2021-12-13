3f81ec64eb0d46aaeb8c903f3bb3504e71ebc243
brandonestrada <65381980+BrEstrada@users.noreply.github.com>
2021-12-12 22:50:40 -0700

    added recycler view

--- app/src/main/java/ca/nait/dmit2504/bestrada/androidroomsample/MainActivity.java
+++ app/src/main/java/ca/nait/dmit2504/bestrada/androidroomsample/MainActivity.java
@@ -1,6 +1,8 @@
 package ca.nait.dmit2504.bestrada.androidroomsample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Add recycler view
+        RecyclerView recyclerView = findViewById(R.id.activity_main_recyclerview);
+        final BinListAdapter adapter = new BinListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
